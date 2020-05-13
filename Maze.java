b3703f900a635c2f7ec2ac114a4f1701e55dbc27
Dodobad <56236646+Dodobad@users.noreply.github.com>
2020-05-13 17:00:20 -0400

    BoilerPlate
    Added boilerplate for GUI and logic

---
+++ Maze.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Maze extends Application {
+  
+  public static void main(String[] args){
+    launch(args);
+  }
+
+  public void start(Stage primaryStage) throws Exception {
+    mazeLogic(primaryStage);
+  }
+
+  public void mazeLogic(Stage stage){
+    VBox box = new VBox();
+    Scene scene = new Scene(box,1000,1000,Color.WHITESMOKE);
+    stage.setTitle("Sequenced Maze Generation");
+    stage.setScene(scene);
+    stage.show();
+  }
+}
